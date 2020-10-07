dc69f07f633fc3931513d307adc09d353e26c92b
Sampath Kumar CHALLA <sampathkumarchalla1@gmail.com>
2020-10-07 11:53:22 +0200

    mixed up components.....time to split up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  element = [];
+  serverElements = [];
+  newServerName = '';
+  newServerContent='';
   title = 'sample-bootstrap';
+
+  onAddServer(){
+    this.serverElements.push({
+      type:'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+    onAddBlueprint(){
+      this.serverElements.push({
+        type:'blueprint',
+        name: this.newServerName,
+        content: this.newServerContent
+      });
+  }
+
 }
